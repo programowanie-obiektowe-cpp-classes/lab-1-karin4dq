aa6fdbc02388763aae2212d05391ee6bdad094d9
Karina Dudko <133782806+karin4dq@users.noreply.github.com>
2025-10-24 08:32:41 +0200

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -57,30 +57,3 @@ public:
 	}
 
 };
-
-
-int main()
-{
-	Wektor2D v1{};
-	v1.setX(1.);             
-	v1.setY(1.);
-
-	double x1 = v1.getX();
-	double y1 = v1.getY();
-
-	//std::println("SPRAWDZENIE 2 -> Wartosc x1: {}, Wartosc y1: {}.", x1, y1);
-
-
-	Wektor2D v2{ 2., 2. };
-	//std::println("SPRAWDZENIE 3 -> Wartosc x2: {}, Wartosc y2: {}.", v2.getX(), v2.getY());
-
-	Wektor2D sum = v1 + v2;
-	//std::println("SPRAWDZENIE 4 -> Wynik sumowania: x3: {}, y3: {}.", sum.getX(), sum.getY());
-
-	double prod = v1 * v2;
-	//std::println("SPRAWDZENIE 5 -> Wartosc mnozenia skalarnego: {}", prod);
-
-
-	return 0;
-}
-
